dba73c8ffd43423c3c1e553d1b02f388ccdeb9f5
Himanshu-v18 <hv11072004@gmail.com>
2025-06-20 15:26:32 +0530

    Add Updated Foody Zone Project

--- Foody Zone/server/src/index.ts
+++ Foody Zone/server/src/index.ts
@@ -12,49 +12,50 @@ app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.get("/", (req, res) => {
   console.log(path.join(__dirname, "../public"));
   const foodData = [
-    {
-      name: "Boilded Egg",
-      price: 10,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/egg.png",
-      type: "breakfast",
-    },
-    {
-      name: "RAMEN",
-      price: 25,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/ramen.png",
-      type: "lunch",
-    },
-    {
-      name: "GRILLED CHICKEN",
-      price: 45,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/chicken.png",
-      type: "dinner",
-    },
-    {
-      name: "CAKE",
-      price: 18,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/cake.png",
-      type: "breakfast",
-    },
-    {
-      name: "BURGER",
-      price: 23,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/burger.png",
-      type: "lunch",
-    },
-    {
-      name: "PANCAKE",
-      price: 25,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      image: "/images/pancake.png",
-      type: "dinner",
-    },
-  ];
+  {
+    name: "Boiled Egg",
+    price: 20,
+    text: "A simple and healthy option, perfect for a quick breakfast or protein boost.",
+    image: "/images/egg.png",
+    type: "breakfast",
+  },
+  {
+    name: "RAMEN",
+    price: 120,
+    text: "A warm bowl of flavorful noodles with rich broth and toppings to satisfy your hunger.",
+    image: "/images/ramen.png",
+    type: "lunch",
+  },
+  {
+    name: "GRILLED CHICKEN",
+    price: 220,
+    text: "Juicy grilled chicken, seasoned to perfection, ideal for a hearty dinner.",
+    image: "/images/chicken.png",
+    type: "dinner",
+  },
+  {
+    name: "CAKE",
+    price: 80,
+    text: "A soft and delicious slice of cake, great with tea or as a sweet breakfast treat.",
+    image: "/images/cake.png",
+    type: "breakfast",
+  },
+  {
+    name: "BURGER",
+    price: 150,
+    text: "A classic burger with fresh ingredients and savory flavors, perfect for lunch.",
+    image: "/images/burger.png",
+    type: "lunch",
+  },
+  {
+    name: "PANCAKE",
+    price: 110,
+    text: "Fluffy pancakes served with syrup, a delightful and comforting evening meal.",
+    image: "/images/pancake.png",
+    type: "dinner",
+  },
+];
+
 
   res.json(foodData);
 });
